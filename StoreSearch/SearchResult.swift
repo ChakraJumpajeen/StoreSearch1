2b378aa4a27acb9bd0545de704d10c09babbb7f9
ChakraJumpajeen <chakra309@hotmail.com>
2021-10-02 12:24:34 -0400

    Add a search bar

---
+++ StoreSearch/SearchResult.swift
@@ -0,0 +1,14 @@
+//
+//  SearchResult.swift
+//  StoreSearch
+//
+//  Created by Chakra Jumpajeen on 10/2/21.
+//
+
+import Foundation
+
+
+class SearchResult {
+  var name = ""
+  var artistName = ""
+}
